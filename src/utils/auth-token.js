1597dd1dd56e3c8093ed9f5f1d12249a3b9c2558
trix180 <danilo.bonardi@shinyfrog.net>
2019-04-29 17:02:57 +0200

    more blacklist entries + new token

--- src/utils/auth-token.js
+++ src/utils/auth-token.js
@@ -1,5 +1,14 @@
-const secretToken = 'F794A060-946C-4317-A01B-A50CD5EA8D3C';
+const secretTokens = [
+  'F794A060-946C-4317-A01B-A50CD5EA8D3C',
+  'C93E2FFA-D494-4F7F-904C-2EE20EADE6CB',
+];
 
 export default function isAuthenticated(headers) {
-  return headers['x-api-key'].includes(secretToken);
+  for (let index = 0; index < secretTokens.length; index += 1) {
+    if (headers['x-api-key'].includes(secretTokens[index])) {
+      return true;
+    }
+  }
+
+  return false;
 }
